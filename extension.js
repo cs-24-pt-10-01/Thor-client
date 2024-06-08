6ef6b9feae070336f9fb2a232f53b19f8073bcfd
Villiamfj <40019337+Villiamfj@users.noreply.github.com>
2024-06-08 20:14:32 +0200

    Handling many calls (#7)
    * used setInterval for dataProcessing and removed some commands
    
    * Missing const and flag
    
    * Moved data processing to webview and changed graph drawing to on demand
    
    * Some file handling and resetting tables when new data is collected
    
    * json rename
    
    * Additions to table, print renames
    
    * Update extension.js
    
    * Moved estimation to webview and fixed some errors with writting to file
    
    * Removed console logs
    
    * Resets estimation and added a input field for branch
    
    * endJsonFile sync
    
    * Added titles to table
    
    * Hiding first iteration and renamed overview
    
    * Fixed error with delimiter
    
    ---------
    
    Co-authored-by: Mads <57893682+Mads518d@users.noreply.github.com>

--- extension.js
+++ extension.js
@@ -5,9 +5,10 @@ const fs = require("fs");
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
 
-	let currentPanel = undefined;
+let currentPanel = undefined;
+
+function activate(context) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('thorClient.start', () => {
@@ -37,10 +38,10 @@ function activate(context) {
 				message => {
 					switch (message.command) {
 						case 'startSocket':
-							startSocket(message.host, message.port, message.repo, message.est);
+							startSocket(message.host, message.port, message.repo);
 							return;
 						case 'readFromFile':
-							readFromFile(message.path, message.est);
+							readFromFile(message.path);
 							return;
 					}
 				},
@@ -48,159 +49,51 @@ function activate(context) {
 				context.subscriptions
 			);
 		})
-
 	);
+}
 
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('thorClient.startSocket', (ip_arg, port_arg, repo_arg, est_arg) => {
-			startSocket(ip_arg, port_arg, repo_arg, est_arg);
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('thorClient.UpdateGraph', (id_arg, value_arg) => {
-			if (!currentPanel) { //skal testes uden dette
-				return;
-			}
-			currentPanel.webview.postMessage({ command: 'updateGraph', id: id_arg, value: value_arg });
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('thorClient.AddGraph', (id_arg) => {
-			if (!currentPanel) { //skal testes uden dette
-				return;
-			}
-			currentPanel.webview.postMessage({ command: 'addGraph', id: id_arg });
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('thorClient.UpdateStats', (id_arg, first_arg, acc_arg, per_call_arg, calls_arg) => {
-			if (!currentPanel) { //skal testes uden dette
-				return;
-			}
-			currentPanel.webview.postMessage({ command: 'updateStats', id: id_arg, first: first_arg, acc: acc_arg, per_call: per_call_arg, calls: calls_arg });
-		})
-	);
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('thorClient.SocketClosed', (dict_arg) => {
-			if (!currentPanel) { //skal testes uden dette
-				return;
-			}
-			currentPanel.webview.postMessage({ command: 'SocketClosed', dict: dict_arg });
-		})
-	);
-
+function socketClosed() {
+	if (!currentPanel) {
+		return;
+	}
+	currentPanel.webview.postMessage({ command: 'socketClosed' });
 }
 
 
+const queue = [];
 
-var lastMeasuredValue = -1; //Used to keep track of the last measured value. -1 is not possible
-var lastMeasuredTimestamp = -1; //Used to keep track of the timestamp of the last measured value. -1 is not possible
-var tempList = [];
+function startSocket(host, port, repo) {
+	const writeStream = fs.createWriteStream(__dirname + '/data.json', { flags: 'w' });
+	writeStream.write('[');
 
-function handleDataWrapper(jsonData, shouldEstimate) {
-	if (shouldEstimate == false) {
-		handleData(jsonData)
-	} else {
-		const estimatedJsonData = [];
-		jsonData.forEach(element => {
-			tempList.push(element)
-			const value = element.rapl_measurement.Intel ? element.rapl_measurement.Intel.pkg : element.rapl_measurement.AMD.pkg;
+	let flag = false;
 
-			if (lastMeasuredValue == -1) {
-				lastMeasuredValue = value;
-				lastMeasuredTimestamp = element.process_under_test_packet.timestamp;
-			}
-			else {
-				if (lastMeasuredValue < value) {//a change in value
-					const estimatedValues = estimateValues(tempList);
-					estimatedJsonData.push(...estimatedValues);
-					tempList = []; //Reset
+	const sender = setInterval(async () => {
+		if (flag) {
+			// stop interval
+			clearInterval(sender);
+			for (const val of queue) {
+				const json = JSON.parse(val);
+				currentPanel.webview.postMessage({ command: 'data', data: json });
+				for (const item of json) {
+					writeStream.write(JSON.stringify(item) + ",");
 				}
 			}
-		});
-		handleData(estimatedJsonData);
-	}
-}
-
-function estimateValues(jsonData) {
-	const newestElement = jsonData[jsonData.length - 1]; //The newest element has the new (and higher) energy value.
-	const newestMeasuredTimestamp = newestElement.process_under_test_packet.timestamp; //timestamp of the newest element
-	const newestMeasuredValue = newestElement.rapl_measurement.Intel ? newestElement.rapl_measurement.Intel.pkg : newestElement.rapl_measurement.AMD.pkg; //The value from the newest element
-
-	jsonData.forEach(element => {
-		const currentTimestamp = element.process_under_test_packet.timestamp; //timestamp of the current element
-
-		const timePeriod = newestMeasuredTimestamp - lastMeasuredTimestamp; //The time used from the last element to the newest element.
-		const timePeriodUsed = currentTimestamp - lastMeasuredTimestamp; //The time the current element have used from the timePeriod
-
-		//handle devide by 0 error.
-		let percentTimeUsed = 0;
-		if (timePeriod != 0) {
-			percentTimeUsed = (timePeriodUsed / timePeriod);
+			writeStream.end(() => { endJsonFile(__dirname + '/data.json'); });
+			socketClosed();
 		}
+		else if (queue.length > 0) {
+			const next = queue.shift();
+			const json = JSON.parse(next);
+			await currentPanel.webview.postMessage({ command: 'data', data: json });
 
-		lastMeasuredTimestamp = currentTimestamp; //with the current element being accounted for, the period of available time is now from the current elements timestamp.
-		const valueDiff = newestMeasuredValue - lastMeasuredValue; //The value difference between the old value and the new value.
-		const currentEstimatedValue = lastMeasuredValue + (valueDiff * percentTimeUsed); //The estimated energy used is a percentage of the value difference
-		element.rapl_measurement.Intel ? (element.rapl_measurement.Intel.pkg = currentEstimatedValue) : (element.rapl_measurement.AMD.pkg = currentEstimatedValue); //set new value
-		lastMeasuredValue = currentEstimatedValue; //Set the new previous value
-
-	});
-	return jsonData;
-}
-
-
-var idThreadDict = {};
-var dict = {}; //data about the different measurements
-//dict[0]: Energy used in first iteration
-//dict[1]: total energy used
-//dict[2]: count (amount of emasurements taken)
-//dict[3]: identifier
-
-function handleData(jsonData) {
-	for (const val of jsonData) {
-		const identifier = val.process_under_test_packet.id;
-		const threadId = val.process_under_test_packet.thread_id;
-
-		// checking if the measurement is from an Intel or AMD processor
-		const value = val.rapl_measurement.Intel ? val.rapl_measurement.Intel.pkg : val.rapl_measurement.AMD.pkg;
-
-		const operation = val.process_under_test_packet.operation;
-
-		const key = identifier + threadId;
-		if (operation == "Start") {
-			idThreadDict[key] = value;
-		} else {
-			if (idThreadDict[key] == undefined) {
-				console.log("Start not found for key: " + key);
-				continue
+			for (const item of json) {
+				writeStream.write(JSON.stringify(item) + ",");
 			}
-			const energyUsed = value - idThreadDict[key];
-
-			if (!(identifier in dict)) {
-				vscode.commands.executeCommand('thorClient.AddGraph', identifier);
-				//				   [first, accumulated, amount of times seen, identifier(used for debugging)] //TODO remove debug identifier
-				dict[identifier] = [energyUsed, 0, 0, identifier];
-			}
-
-			vscode.commands.executeCommand('thorClient.UpdateGraph', identifier, energyUsed);
-
-			dict[identifier][1] += energyUsed;
-			dict[identifier][2] += 1;
-			const avg = (dict[identifier][1] / dict[identifier][2]).toFixed(2) // rounded to two decimals
-			vscode.commands.executeCommand('thorClient.UpdateStats', identifier, dict[identifier][0].toFixed(2), dict[identifier][1].toFixed(2), avg, dict[identifier][2]);
 		}
-	}
-}
+	}, 100); // delay for prioritizing accepting data
 
-function startSocket(host, port, repo, shouldEstimate) {
 	const endString = "end"; // string used to indicate end of data by the server
-	const end = new Buffer.from(endString);
 
 	// if no repo, indicating observer by sending "none"
 	if (repo == "") {
@@ -217,23 +110,22 @@ function startSocket(host, port, repo, shouldEstimate) {
 	});
 
 	client.on("data", (data) => {
-		// if the endString is found then parse data else concat to buffer
-		if (data.subarray(data.length - end.length).toString() == endString) {
-			dataBuffer = Buffer.concat([dataBuffer, data]);
+		let string = Buffer.concat([dataBuffer, data]).toString();
+		let splits = string.split("]" + endString); // ending of list + endString
 
-			// removing endString from data
-			const dataBufferString = dataBuffer.toString().slice(0, -end.length);
-
-			const jsonData = JSON.parse(dataBufferString);
-			handleDataWrapper(jsonData, shouldEstimate);
-
-			writeJsonToFile(jsonData, __dirname + '/data.json');
-
-			// clearing buffer
-			dataBuffer = Buffer.alloc(0);
-		}
-		else {
-			dataBuffer = Buffer.concat([dataBuffer, data]);
+		for (let i = 0; i < splits.length; i++) {
+			if (i == splits.length - 1) {
+				if (splits[i] != "") {
+					// saving incomplete data, for next chunk
+					dataBuffer = Buffer.from(splits[i]);
+				}
+				else {
+					// clearing buffer
+					dataBuffer = Buffer.alloc(0);
+				}
+				break;
+			}
+			queue.push(splits[i] + "]");
 		}
 	});
 
@@ -243,32 +135,17 @@ function startSocket(host, port, repo, shouldEstimate) {
 	});
 
 	client.on("close", () => {
-		if (shouldEstimate) {
-			if (tempList.length != 0) {//check if there are any leftover elements
-				handleData(tempList); //since no change has occurd after these elements, it is not possible to estimate their value.
-			}
-		}
-
-		vscode.commands.executeCommand('thorClient.SocketClosed', dict);
-		endJsonFile(__dirname + '/data.json');
+		flag = true;
 		console.log("Connection closed");
 	});
 }
 
-// write data to file, endJsonFile should be called after all date is written
-function writeJsonToFile(data, path = 'data.json') {
-	if (!fs.existsSync(path)) {
-		fs.writeFileSync(path, '[', 'utf8');
-	}
-
-	for (const val of data) {
-		console.log(val);
-		fs.appendFileSync(path, JSON.stringify(val) + ",", 'utf8');
-	}
-}
-
 // ends the json file with a ']'
 function endJsonFile(path = 'data.json') {
+	if (!fs.existsSync(path)) {
+		console.log("no file written");
+		return;
+	}
 	// removing the last comma
 	fs.truncateSync(path, fs.statSync(path).size - 1);
 
@@ -276,21 +153,17 @@ function endJsonFile(path = 'data.json') {
 	fs.appendFileSync(path, ']', 'utf8');
 }
 
-function readFromFile(path, shouldEstimate) {
+function readFromFile(path) {
 	try {
 		const data = fs.readFileSync(path);
 		const jsonData = JSON.parse(data);
-		handleDataWrapper(jsonData, shouldEstimate);
+		currentPanel.webview.postMessage({ command: 'data', data: jsonData });
 
 		// simulating stop
-		if (shouldEstimate) {
-			if (tempList.length != 0) {//check if there are any leftover elements
-				handleData(tempList); //since no change has occurd after these elements, it is not possible to estimate their value.
-			}
-		}
-		vscode.commands.executeCommand('thorClient.SocketClosed', dict);
+		socketClosed();
 	}
 	catch (err) {
+		socketClosed();
 		console.error("failed to read file", err);
 	}
 }
